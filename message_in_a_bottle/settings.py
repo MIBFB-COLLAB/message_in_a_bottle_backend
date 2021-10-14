50f22efb29dd0c0c9e44df1e39a6cb121d01d582
Marla Schulz <marlaa.schulz@gmail.com>
2021-10-14 14:13:58 -0700

    Hide secret key with decouple

--- message_in_a_bottle/settings.py
+++ message_in_a_bottle/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+# import dotenv
 
 from pathlib import Path
 
@@ -19,11 +21,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hk8n!ro&d+so32++k-8v_c(n-stj_*bwi7n6$hxl=dv0(ne!%l'
+#Strategy 1:
+# Add .env variables anywhere before SECRET_KEY
+# dotenv_file = os.path.join(BASE_DIR, ".env")
+# if os.path.isfile(dotenv_file):
+#     dotenv.load_dotenv(dotenv_file)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# UPDATE secret key
+# SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
+
+#Strategy 2:
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production! Is debug like pry?
+#TODO anytime adding new feature, change to true.
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -131,3 +145,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
