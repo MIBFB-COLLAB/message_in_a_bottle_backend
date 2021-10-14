bbd25dfc0476d075feb931bcbc7b1dfc0474cf3a
Taylor Varoglu <tava8398@colorado.edu>
2021-10-13 19:08:59 -0600

    Initial commit

---
+++ message_in_a_bottle/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for message_in_a_bottle project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'message_in_a_bottle.settings')
+
+application = get_wsgi_application()
