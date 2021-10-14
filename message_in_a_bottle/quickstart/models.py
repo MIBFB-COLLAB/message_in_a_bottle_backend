882568f025ecea40d459d560c8e4f383873131b2
Taylor Varoglu <tava8398@colorado.edu>
2021-10-13 19:55:22 -0600

    Add Story to quickstart/models

--- message_in_a_bottle/quickstart/models.py
+++ message_in_a_bottle/quickstart/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Story(models.Model)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    message = models.TextField()
+    # optional name of user (can be anonymous)
+    name = models.CharField()
