3dfe58753c619543ec693117b9105ac26097609b
Marla Schulz <marlaa.schulz@gmail.com>
2021-10-14 14:39:39 -0700

    Get rid of spaces

--- message_in_a_bottle/settings.py
+++ message_in_a_bottle/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-
 import os
 # import dotenv
 
