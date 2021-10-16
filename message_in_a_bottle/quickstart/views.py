e32dc7e31f5b8bcb565c3d1979781a479b057b85
Taylor Varoglu <tava8398@colorado.edu>
2021-10-15 19:04:02 -0600

    Add methods to views

--- message_in_a_bottle/quickstart/views.py
+++ message_in_a_bottle/quickstart/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from snippets.models import Story
+from snippets.serializers import StorySerializer
 
 # Create your views here.
+# TODO: remove @csrf_exempt line after tutorial / troubleshooting completed
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all stories, or create a new story.
+    """
+    if request.method == 'GET':
+        stories = Story.objects.all()
+        serializer = StorySerializer(stories, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = StorySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+# TODO: remove @csrf_exempt line after tutorial / troubleshooting completed
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a story.
+    """
+    try:
+        story = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = StorySerializer(story)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = StorySerializer(story, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        story.delete()
+        return HttpResponse(status=204)
