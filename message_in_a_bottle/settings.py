483d41c6c78f31d8d1097320dcf79c62225d0888
Taylor Varoglu <tava8398@colorado.edu>
2021-10-17 19:00:00 -0600

    Rename postgres db for circle builds

--- message_in_a_bottle/settings.py
+++ message_in_a_bottle/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'message_in_a_bottle',
         'USER': 'defaultuser',
         'PASSWORD': 'password',
         'HOST': '127.0.0.1',
