09fcb95b02e53ccb71f83fde646cc76f8657a944
Taylor Varoglu <tava8398@colorado.edu>
2021-10-17 20:13:26 -0600

    Add edge case for get_story request spec

--- message_in_a_bottle/api/tests/test_requests.py
+++ message_in_a_bottle/api/tests/test_requests.py
@@ -1,21 +1,56 @@
 import pytest
 from rest_framework.test import APIClient
-from message_in_a_bottle.api.views import StoryList
+from message_in_a_bottle.api.models import Story
 from message_in_a_bottle.api.serializers import StorySerializer
 
 @pytest.mark.django_db
-def test_get_story():
+def test_get_existing_story():
     story = {
-        'id': 1,
+        'title': 'My Cool Story',
+        'message': 'I once saw a really pretty flower.',
+        'latitude': 123.456892,
+        'longitude': -19.982791
+    }
+    assert Story.objects.count() == 0
+
+    Story.objects.create(
+        title=story['title'],
+        message=story['message'],
+        latitude=story['latitude'],
+        longitude=story['longitude']
+    )
+    assert Story.objects.count() == 1
+
+    id = Story.objects.latest('id').id
+    route = f'/api/v1/stories/{id}'
+
+    client = APIClient()
+    response = client.get(route)
+    serializer = StorySerializer(Story.objects.get(pk=id))
+
+    assert response.status_code == 200
+    assert response.data['data'] == serializer.data
+
+@pytest.mark.django_db
+def test_get_non_existent_story():
+    story = {
         'title': 'My Cool Story',
         'message': 'I once saw a really pretty flower.',
         'latitude': 123.456892,
         'longitude': -19.982791
     }
 
-    route = '/api/v1/stories'
+    Story.objects.create(
+        title=story['title'],
+        message=story['message'],
+        latitude=story['latitude'],
+        longitude=story['longitude']
+    )
+
+    id = Story.objects.latest('id').id + 1
+    route = f'/api/v1/stories/{id}'
 
     client = APIClient()
     response = client.get(route)
 
-    assert response.status_code == 200
+    assert response.status_code == 404
