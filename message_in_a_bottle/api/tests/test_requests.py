20b649250afba2c100230493309a24989f6574ec
Taylor Varoglu <tava8398@colorado.edu>
2021-10-18 11:26:13 -0600

    Refactor test_requests.test_db_setup() to use Class method

--- message_in_a_bottle/api/tests/test_requests.py
+++ message_in_a_bottle/api/tests/test_requests.py
@@ -1,29 +1,31 @@
 import pytest
+from django.test import TestCase
 from rest_framework.test import APIClient
 from django.urls import reverse
 from message_in_a_bottle.api.models import Story
 from message_in_a_bottle.api.serializers import StorySerializer
 
 @pytest.mark.django_db
-class TestGetStory:
-    def test_db_setup(self):
+class TestGetStory(TestCase):
+    @classmethod
+    def test_db_setup(cls):
         assert Story.objects.count() == 0
-        self.story_dict = {
+        cls.story_dict = {
             'title': 'My Cool Story',
             'message': 'I once saw a really pretty flower.',
             'latitude': 123.456892,
             'longitude': -19.982791
         }
-        self.new_story = Story.objects.create(
-            title = self.story_dict['title'],
-            message = self.story_dict['message'],
-            latitude = self.story_dict['latitude'],
-            longitude = self.story_dict['longitude']
+        cls.new_story = Story.objects.create(
+            title = cls.story_dict['title'],
+            message = cls.story_dict['message'],
+            latitude = cls.story_dict['latitude'],
+            longitude = cls.story_dict['longitude']
         )
         assert Story.objects.count() == 1
 
     def test_get_existing_story(self):
-        TestGetStory.test_db_setup(self)
+        TestGetStory.test_db_setup()
 
         self.valid_id = Story.objects.latest('id').id
         self.route = f'/api/v1/stories/{self.valid_id}'
@@ -36,7 +38,7 @@ class TestGetStory:
         assert response.data['data'] == serializer.data
 
     def test_get_non_existent_story(self):
-        TestGetStory.test_db_setup(self)
+        TestGetStory.test_db_setup()
 
         self.invalid_id = Story.objects.latest('id').id + 1
         self.route = f'/api/v1/stories/{self.invalid_id}'
