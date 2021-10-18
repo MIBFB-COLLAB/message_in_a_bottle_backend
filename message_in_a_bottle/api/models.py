9456842b939abd96011ba504a7f389dc976c2c43
Taylor Varoglu <tava8398@colorado.edu>
2021-10-18 13:15:50 -0600

    Add model migrations for timestamps and max_length=1000 chars for message column, add new fields to serializer, update Postman collection

--- message_in_a_bottle/api/models.py
+++ message_in_a_bottle/api/models.py
@@ -5,18 +5,20 @@ from django.db import models
 class Story(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
-    message = models.TextField()
+    message = models.TextField(max_length=1000)
     # optional name of user (can be anonymous)
     name = models.CharField(max_length=50, default='Anonymous')
     title = models.CharField(max_length=50, default='My Story')
     location = models.CharField(max_length=50, default='')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def create_dict(story):
         return {
-            "key": story["id"],
-            "title": story["title"],
-            "shapePoints": [
-                story["latitude"],
-                story["longitude"]
+            'key': story['id'],
+            'title': story['title'],
+            'shapePoints': [
+                story['latitude'],
+                story['longitude']
             ]
         }
