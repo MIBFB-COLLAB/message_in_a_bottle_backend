e76e2b5a37c6439ffc53d613704693be9be038b3
Taylor Varoglu <tava8398@colorado.edu>
2021-10-18 13:27:39 -0600

    Reorder attributes in Story model

--- message_in_a_bottle/api/models.py
+++ message_in_a_bottle/api/models.py
@@ -5,10 +5,10 @@ from django.db import models
 class Story(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
-    message = models.TextField(max_length=1000)
     # optional name of user (can be anonymous)
     name = models.CharField(max_length=50, default='Anonymous')
     title = models.CharField(max_length=50, default='My Story')
+    message = models.TextField(max_length=1000)
     location = models.CharField(max_length=50, default='')
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
