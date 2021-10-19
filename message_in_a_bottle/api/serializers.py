a10d496ebdd14cfab5ddb22a11289c68bba01179
Taylor Varoglu <tava8398@colorado.edu>
2021-10-19 13:39:19 -0600

    Add sserializer test for reformat function

--- message_in_a_bottle/api/serializers.py
+++ message_in_a_bottle/api/serializers.py
@@ -6,5 +6,18 @@ class StorySerializer(serializers.ModelSerializer):
         model = Story
         fields = ['id', 'latitude', 'longitude', 'message', 'name', 'title', 'location', 'created_at', 'updated_at']
 
-        def reformat(self, story):
-            output = {}
+    def reformat(story):
+        return {
+            'id': story.id,
+            'type': story.__class__.__name__,
+            'attributes': {
+                'name': story.name,
+                'title': story.title,
+                'message': story.message,
+                'latitude': story.latitude,
+                'longitude': story.message,
+                'location': story.location,
+                'created_at': story.created_at,
+                'updated_at': story.updated_at
+            }
+        }
