3ee9aac8622010871cb203cec22df52c381f7568
Taylor Varoglu <tava8398@colorado.edu>
2021-10-20 10:24:57 -0600

    Complete request_test happy and sad path for POST request

--- message_in_a_bottle/api/tests/test_requests.py
+++ message_in_a_bottle/api/tests/test_requests.py
@@ -6,9 +6,9 @@ from message_in_a_bottle.api.models import Story
 from message_in_a_bottle.api.serializers import StorySerializer
 
 @pytest.mark.django_db
-class TestGetStory(TestCase):
+class TestStoryRequests(TestCase):
     @classmethod
-    def test_db_setup(cls):
+    def test_db_setup(cls, return_dict=False):
         assert Story.objects.count() == 0
         cls.story_dict = {
             'title': 'My Cool Story',
@@ -23,9 +23,11 @@ class TestGetStory(TestCase):
             longitude = cls.story_dict['longitude']
         )
         assert Story.objects.count() == 1
+        if return_dict:
+            return cls.story_dict
 
     def test_get_existing_story(self):
-        TestGetStory.test_db_setup()
+        TestStoryRequests.test_db_setup()
 
         self.valid_id = Story.objects.latest('id').id
         self.route = f'/api/v1/stories/{self.valid_id}'
@@ -38,7 +40,7 @@ class TestGetStory(TestCase):
         assert response.data['data'] == serializer.reformat(serializer.data)
 
     def test_get_non_existent_story(self):
-        TestGetStory.test_db_setup()
+        TestStoryRequests.test_db_setup()
 
         self.invalid_id = Story.objects.latest('id').id + 1
         self.route = f'/api/v1/stories/{self.invalid_id}'
@@ -47,3 +49,30 @@ class TestGetStory(TestCase):
         response = client.get(self.route)
 
         assert response.status_code == 404
+
+    def test_create_valid_story(self):
+        self.story_dict = TestStoryRequests.test_db_setup(return_dict=True)
+        self.route = '/api/v1/stories'
+
+        client = APIClient()
+        response = client.post(self.route, self.story_dict, format='json')
+        story_id = response.data['data']['id']
+        serializer = StorySerializer(Story.objects.get(pk=story_id))
+
+        assert response.status_code == 201
+        assert response.data['data'] == serializer.reformat(serializer.data)
+        assert Story.objects.count() == 2
+
+    def test_create_empty_story(self):
+        self.required_keys = ['latitude', 'longitude', 'message']
+        self.route = '/api/v1/stories'
+
+        client = APIClient()
+        response = client.post(self.route, {}, format='json')
+        errors = response.data['errors']
+
+        assert response.status_code == 400
+        assert len(errors.keys()) == 3
+        
+        for r in self.required_keys:
+            assert r in errors
