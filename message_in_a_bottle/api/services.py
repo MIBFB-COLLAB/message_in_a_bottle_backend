a5abb16a687cb25672f05403a6e7478b0f7fc8ec
Matt Kragen <mattdkragen@gmail.com>
2021-10-21 15:13:15 -0700

    Change syntax to not pass Story object to service

--- message_in_a_bottle/api/services.py
+++ message_in_a_bottle/api/services.py
@@ -27,25 +27,12 @@ class MapService():
         response = requests.post(url, params=params, data=json.dumps(data, indent = 1))
         return response.json()
     
-    def get_distance(lat, long, story):
+    def get_distance(lat, long, story_lat, story_long):
       url = 'http://www.mapquestapi.com/directions/v2/route'
       params = {
-        "key": os.environ.get('MAPQUEST_KEY')
+        "key": os.environ.get('MAPQUEST_KEY'),
+        "from": f"{lat},{long}",
+        "to": f"{story_lat},{story_long}"
       }
-      data = {
-        "from": {
-          "latLng": {
-            "lat": lat,
-            "lng": long
-          }
-        },
-        "to": {
-          "latLng": {
-            "lat": story['latitude'],
-            "lng": story['longitude']
-          }
-        },
-        "remoteDataList": story
-      }
-      response = requests.post(url, params=params, data=json.dumps(data, indent = 1))
+      response = requests.get(url, params=params)
       return response.json()
--- message_in_a_bottle/api/views.py
+++ message_in_a_bottle/api/views.py
@@ -51,6 +51,7 @@ class StoryDetail(APIView):
     """
     def get(self, request, pk, format=None):
         story = self.get_object(pk)
+        MapService.get_distance(float(request.query_params['latitude']), float(request.query_params['longitude']), story)
         serializer = StorySerializer(story)
         return Response({'data':serializer.reformat(serializer.data)})
     """
