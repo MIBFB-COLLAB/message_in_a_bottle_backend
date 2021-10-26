d0f0d558273f3c1e0c17ab0edf89426b4ce2afed
Matt Kragen <mattdkragen@gmail.com>
2021-10-26 16:01:24 -0700

    Cleanup

--- message_in_a_bottle/conftest.py
+++ message_in_a_bottle/conftest.py
@@ -8,9 +8,4 @@ def vcr_config():
         "filter_query_parameters": [('key', 'DUMMY')],
         "filter_post_data_parameters": [('key', 'DUMMY')]
     }
-
-# with my_vcr.use_cassette('test.yml', filter_query_parameters=['api_key']):
-#     requests.get('http://api.com/getdata?api_key=secretstring')
-
-# with my_vcr.use_cassette('test.yml', filter_post_data_parameters=['client_secret']):
-#     requests.post('http://api.com/postdata', data={'api_key': 'secretstring'})
+    
